60e76fee3a59c404fd04e2c65a8db62bbc8086b7
Documenter.jl <documenter@juliadocs.github.io>
2022-01-25 17:43:24 +0000

    build based on 4bcb2fb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BitempWebApp.jl","page":"BitempWebApp.jl","title":"BitempWebApp.jl","text":"","category":"section"},{"location":"","page":"BitempWebApp.jl","title":"BitempWebApp.jl","text":"Documentation for BitempWebApp.jl","category":"page"}]
+}
